cf5df228a5a8f4e1d6bf05fc9405938fd8d2d263
Safayatcc20 <mdsafayatcc20@gmail.com>
2024-07-14 20:45:28 +0600

    safayat

---
+++ src/com/toedter/calendar/demo/FontSets.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.toedter.calendar.demo;
+
+import java.awt.Font;
+
+/**
+ *
+ * @author Saif
+ */
+class FontSets {
+
+    static FontSet createDefaultFontSet(Font font) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/org/xemocode/demo/DriverManager.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.xemocode.demo;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Saif
+ */
+class DriverManager {
+
+    static Connection getConnection;
+
+    static Connection getConnection(String jdbcmyqllocalhostutility_service_provider, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
